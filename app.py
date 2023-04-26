f77cadb1fa4d7061461200072b5abdd3d09f43de
Paul Delta <117391498+DeltaPaul4@users.noreply.github.com>
2023-04-27 00:43:34 +0700

    update model with accuracy up to 98%

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import gradio as gr
 import pickle
 import numpy as np
 
-model = pickle.load(open('model.pickle', 'rb'))
+model = pickle.load(open('model_RMSprop.pickle', 'rb'))
 
 def recognize_digit(input):
-  input = input.reshape((1,28,28))
+  input = input.reshape((1,784))
   
   prediction = np.squeeze(model.predict(input))
   label = [0,1,2,3,4,5,6,7,8,9]
